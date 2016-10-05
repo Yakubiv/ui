02b7139a21b680cdb0dd6f9a58ff04cafcf27779
Petro <petro.yakubiv@gmail.com>
2016-10-05 20:42:15 +0300

    Finalize third part

--- app/routes/auth/register.js
+++ app/routes/auth/register.js
@@ -3,10 +3,13 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   actions: {
     doRegister() {
-      alert('registration attempted');
-    },
-    model() {
-      return this.store.createRecord('user');
+      this.get('currentModel').save()
+        .then(() => {
+          this.transitionTo('auth.login');
+        });
     }
+  },
+  model() {
+    return this.store.createRecord('user');
   }
 });
--- config/environment.js
+++ config/environment.js
@@ -16,6 +16,10 @@ module.exports = function(environment) {
     APP: {
       // Here you can pass flags/options to your application instance
       // when it is created
+    },
+    DS: {
+     host: 'http://localhost:4000',
+     namespace: 'api'
     }
   };
 
@@ -39,7 +43,7 @@ module.exports = function(environment) {
   }
 
   if (environment === 'production') {
-
+    ENV.DS.host = 'https://arcane-temple-31463.herokuapp.com/';
   }
 
   return ENV;
